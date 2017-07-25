b22507076fca7232842759ca44034017f1aa2ffe
Don Shults <don.shults@neudesic.com>
2017-07-25 11:44:30 -0700

    Create Zip File

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ var gulp = require('gulp');
 var lambda = require('gulp-awslambda');
 var zip = require('gulp-zip')
 
-var lambda_params = {
+var lambda_params1 = {
 	FunctionName: 'getInventory',
 	Role: 'arn:aws:iam::615275379173:role/lambda_basic_execution'
 };
-
+var lambda_params2 = {
+	FunctionName: 'getOderStatus',
+	Role: 'arn:aws:iam::615275379173:role/lambda_basic_execution'
+};
 var opts = {
 	region: 'us-west-2'
 };
 gulp.task('default',function(){
-    return gulp.src(['*.js','node_modules/**'] )
+    return gulp.src(['*.js','node_modules/**/*.js'] )
         .pipe(zip('getOrder.zip'))
-        .pipe(lambda(lambda_params, opts))
+        //.pipe(lambda(lambda_params, opts))
         .pipe(gulp.dest('.'));
 });
 
