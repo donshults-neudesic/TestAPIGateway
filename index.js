6898e3c7799eda114fe5eef22255edee52e6af6d
Don Shults <don.shults@neudesic.com>
2017-08-07 11:49:18 -0700

    Change Function

--- index.js
+++ index.js
@@ -1,67 +1,34 @@
-var faker = require('faker');
-var AWS = require("aws-sdk/dist/aws-sdk");
-var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
+console.log('Loading event');
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-exports.handler = function(event, context, callback){
-var table = "Movies";
-var params = {
-  TableName: table,
-  Item: {
-    "movieId": Math.floor((Math.random() * 100) + 1),
-    "movieName": "GhostBusters"
-  }
-}
+exports.handler = function(event, context) {
+    console.log(JSON.stringify(event, null, '  '));
+    dynamodb.listTables(function(err, data) {
+      console.log(JSON.stringify(data, null, '  '));
+    });
+    var tableName = "chat";
+    var datetime = new Date().getTime().toString();
+    dynamodb.putItem({
+        "TableName": tableName,
+        "Item" : {
+            "user": {"S": event.user },
+            "date": {"S": datetime },
+            "msg": {"S": event.msg}
+        }
+    }, function(err, data) {
+        if (err) {
+            context.done('error','putting item into dynamodb failed: '+err);
+        }
+        else {
+            console.log('great success: '+JSON.stringify(data, null, '  '));
+            context.done('K THX BY');
+        }
+    });
+};
 
-  docClient.put(params,function(err,data){
-    if(err){
-      callback(err,null);
-    }else{
-      callback(null,data);
-    }
-  });
-  //callback(null,movie);
-  //context.succeed(inventory);
-}
-
-function UpdateDb(movie){
-
-
-}
-
-function getMyShoe() {
-  var shoeType = [
-    "running shoe",
-    "cross-training shoe",
-    "tennis shoe",
-    "basketball shoe",
-    "aerobic shoe",
-    "spinning shoe"
-  ]
-  return shoeType[getNum(0, 5)];
-}
-
-function getShoeName(myShoe){
-  return faker.company.catchPhraseNoun() + " " + faker.company.catchPhraseDescriptor() + " " + myShoe;
-}
-
-function getShoeColor() {
-  return faker.commerce.color();
-}
-
-function getShoeDescription(myShoe) {
-  return "A(n) " + faker.commerce.productAdjective() + ", " + faker.commerce.productAdjective() +
-  " " + myShoe + " made from the finest " + faker.commerce.productMaterial() +
-  " designed for the " + faker.company.bsBuzz() + " individual!"
-}
-
-function getShoeSize() {
-  return getNum(1, 13);
-}
-
-function getShoePrice() {
-  return faker.commerce.price();
-}
-
-function getNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// sample event
+//{
+//  "user": "bart",
+//  "msg": "hey otto man"
+//}
